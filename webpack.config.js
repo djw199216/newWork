a4af1a628982bafdd45f24854a1c57e268ba9fbc
djw19921006 <djw19921006@163.com>
2018-12-17 16:38:57 +0800

    bbb

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 var ExtractTextPlugin  = require('extract-text-webpack-plugin');
 // var HtmlWebpackPlugin = require('html-webpack-plugin');
-var extractCss = new ExtractTextPlugin("css/a.css");//这里的参数是配置编译后的css路径和文件名,相对于output里的path选项
+var extractCss = new ExtractTextPlugin("./src/css/style.css");//这里的参数是配置编译后的css路径和文件名,相对于output里的path选项
 
 module.exports = {
     entry:"./src/index.js",
@@ -30,6 +30,25 @@ module.exports = {
             {
                 test: /\.scss$/,
                 loader: ExtractTextPlugin.extract("style", 'css!sass')
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract("style", 'css!sass')
+                //这里用了样式分离出来的插件，如果不想分离出来，可以直接这样写 loader:'style!css!sass'
+            },
+            {
+                test: /\.scss/,
+                use: ExtractTextPlugin.extract({
+                    use:[
+                        {
+                            loader:'css-loader'
+                        },
+                        {
+                            loader:'sass-loader'
+                        }
+                    ],
+                    fallback:'style-loader'
+                })
             }
         ]
     },
